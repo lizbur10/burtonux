b7c701fb95650dfb188e1daf21928fcfbf9d413b
Liz Burton <liz.burton147@gmail.com>
2017-03-22 16:41:54 -0400

    Refactoring of CSS

---
+++ PUBLIC/index.php
@@ -0,0 +1,59 @@
+<?php
+include("header.html");
+?>
+
+			<section class="main">
+				<h1 class="visually-hidden">Liz Burton's Home Page</h1>
+				<h2>About Me</h2>
+	 			<p>I am an experienced UX professional and tenacious user advocate with a solid understanding of UX design principles. I also have front-end development skills and my understanding of development implications gives me additional insight into the design process. </p>
+	 			<p>My skills and background include: </p>
+				<ul class="triangle-list">
+					<li> a deep understanding of user experience and interaction design</li>
+					<li> hands-on experience with a wide array of research methods</li>
+					<li> excellent communication and collaboration skills</li>
+					<li> a proven record of providing actionable design recommendations based on best practice, user and business needs, and thoughtful user research</li>
+				</ul>
+				<p>On this site, you can see <a class="text-link" href="ux-projects.html">examples of my research and design work</a> and <a class="text-link" href="dev-projects.html">some of my development projects</a>. You can also <a class="text-link" href="resume.html">view my resume</a>.</p>
+	<!-- 			<p>My skillset includes:</p>
+				<ul>
+					<li>User research</li>
+					<li>Information Architecture</li>
+					<li>UX Design</li>
+					<li>Front-end Development:
+						<ul>
+							<li>HTML5/CSS3</li>
+							<li>Javascript/JQuery</li>
+							<li>Custom theming in Wordpress; PHP</li>
+							<li>Responsive design</li>
+						</ul>
+					</li>
+				</ul>
+
+	 -->		
+
+	</section>
+
+			<footer>
+<!-- 				<ul>
+					<li id="email"><a href="mailto:liz.burton147@gmail.com"><span>liz.burton147@gmail.com</span></a></li>
+					<li id="linkedin"><a href="https://www.linkedin.com/in/burtonux" target="_blank"><span>www.linkedin.com/in/burtonux</span></a></li>
+					<li id="phone"><span>(773) 710-8686</span></li>
+				</ul>
+	 -->			<div>
+					<p class="copyright">&copy; Liz Burton 2017</p>
+					<p>Site <a href="dev-projects.html#burtonux" class="text-link">designed and developed</a> by Liz Burton</p>
+				</div>
+			</footer>
+			<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			  ga('create', 'UA-75714954-1', 'auto');
+			  ga('send', 'pageview');
+
+			</script>
+		</div>
+	</body>
+</html>
