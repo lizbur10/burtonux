5d2696a32558a2e5a32f3e643eec08bddf31181f
lizbur10 <liz.burton147@gmail.com>
2018-11-29 15:27:41 -0500

    modified gitignore file

---
+++ XXdontUpload/dev-sanctuary.php
@@ -0,0 +1,41 @@
+<?php
+include("header.php");
+?>
+
+		<section id="content" class="project">
+<!-- 			<div class="navlinks">
+				<p class="navlink-left"><a class="text-link" href="<?php echo $page_info['prev_link']; ?>">&laquo; <?php echo $page_info['prev_link_label']; ?></a></p>
+				<p class="navlink-right"><a class="text-link" href="<?php echo $page_info['next_link']; ?>"><?php echo $page_info['next_link_label']; ?> &raquo;</a></p>
+			</div>
+ -->
+			<div class="project-header">
+				<h2>Class Project: Sanctuary Site</h2>
+				<h4>WordPress custom theme, HTML/CSS, MAMP</h4>
+			</div>
+			<figure class="right">
+				<a href="http://burtonux.com/sanctuary/" target="_blank" ><img class="add-border" src="img/projects/sanctuary.jpg" alt="Sanctuary Custom Theme"></a>
+			</figure>
+
+			<div>
+				<h3>The Project</h3>
+				<p>As a class project for the Minneapolis College of Art and Design WordPress/PHP class, I created the home page and one internal page of a site using WordPress. </p>
+				<h3>Development Details</h3>
+				<p>I used the following techniques to develop the site:</p>
+				<ul class="triangle-list">
+					<li>Custom page templates for the home page and internal page</li>
+					<li>Use of a Javascript plugin to create the image carousel on the internal page</li>
+					<li>Designing to Photoshop comps</li>
+				</ul>
+				<p>The page was designed for a screen resolution of 1280 pixels.</p>
+				<p><a class="text-link" href="http://burtonux.com/sanctuary/" target="_blank">Link to assignment</a></p>
+				<p><a class="text-link" href="https://github.com/lizbur10/sanctuary" target="_blank">Github repo</a></p>
+			</div>
+
+			
+<!-- 			<div class="navlinks">
+				<p class="navlink-left"><a class="text-link" href="<?php echo $page_info['prev_link']; ?>">&laquo; <?php echo $page_info['prev_link_label']; ?></a></p>
+				<p class="navlink-right"><a class="text-link" href="<?php echo $page_info['next_link']; ?>"><?php echo $page_info['next_link_label']; ?> &raquo;</a></p>
+			</div>
+ -->		</section>
+
+<?php include("footer.php"); ?>
