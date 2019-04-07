4985258cdc50c9a93dbde8280e4b6c28624ce5a0
lizbur10 <liz.burton147@gmail.com>
2019-04-07 18:18:57 -0400

    wired up new blog post

--- PUBLIC/functions.php
+++ PUBLIC/functions.php
@@ -113,8 +113,19 @@ function get_page_info($current_page) {
 			"menu_label" => "Blog", 
 			"page_title" => "Liz Burton Blog",
 			"sub_of" => "blog.php",
-			"prev_link" => "blog-032419.php",
+			"prev_link" => "blog-033119.php",
 			"prev_link_label" => "previous post"
+		),
+		array(
+			"page" => "blog-033119.php",
+			"add_to_menu" => false,
+			"menu_label" => null, 
+			"page_title" => "Liz Burton Blog",
+			"sub_of" => "blog.php",
+			"prev_link" => "blog-032419.php",
+			"prev_link_label" => "previous post",
+			"next_link" => "blog.php",
+			"next_link_label" => "next post"
 		),
 		array(
 			"page" => "blog-032419.php",
@@ -124,7 +135,7 @@ function get_page_info($current_page) {
 			"sub_of" => "blog.php",
 			"prev_link" => "blog-031719.php",
 			"prev_link_label" => "previous post",
-			"next_link" => "blog.php",
+			"next_link" => "blog-033119.php",
 			"next_link_label" => "next post"
 		),
 		array(
